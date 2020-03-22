5ef0c8530aa557d019bdb7f9319cc590d14f824f
javier mugueta <javiermugueta@MacBook-Pro.local>
2020-03-23 00:32:44 +0100

    .

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## 
 Example
 ```
-rm ocideleteoldsnapshots.sh
+rm oci_fs_snapshot_with_retention.sh
 wget https://raw.githubusercontent.com/javiermugueta/rawcontent/master/ocideleteoldsnapshots.sh
 chmod 700 ocideleteoldsnapshots.sh 
 ./ocideleteoldsnapshots.sh ocid1.filesystem.oc1.eu_frankfurt_1.aaaaaaaaaaab5orimzzgcllqojxwiotfouwwm4tbnzvwm5lsoqwtcllbmqwtgaaa 60
---
+++ oci_fs_snapshot_with_retention.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# jmu mar/22/2020
+# creates snapshot
+# deletes snapshots of file system with id argument $1 older than the number of days passed as $2 argument
+#
+filesystem_id=$1 #"ocid1.filesystem.oc1.eu_frankfurt_1.aaaaaaaaaaab5orimzzgcllqojxwiotfouwwm4tbnzvwm5lsoqwtcllbmqwtgaaa"
+numdays=$2
+limitdate=$(date -d "-$numdays days" +"%Y-%m-%d %H:%M:%S")
+echo "Will delete snapshots older than "$limitdate
+# list of snapshots
+sninfo=`oci fs snapshot list --file-system-id $filesystem_id`
+sninfo="${sninfo//time-created/time_created}"
+#echo $sninfo
+count=`echo $sninfo | jq '.data | length'`
+count=`expr $count - 1`
+# looping records
+for i in $(eval echo {0..$count})
+do
+    name=`echo $sninfo | jq .data[$i].name`
+    name=$(eval echo $name)
+    id=`echo $sninfo | jq .data[$i].id`
+    id=$(eval echo $id)
+    created=`echo $sninfo | jq .data[$i].time_created`
+    created=$(eval echo $created)
+    #echo "Snapshot name: "$name
+    #echo "Snapshot id: "$id
+    #echo "Snapshot creation date: "$created
+    if [[ $created < $limitdate ]]; then
+        echo "Deleting snapshot $name"
+        oci fs snapshot delete --snapshot-id $id --force
+    fi
+done
+echo "Finished, have a good day!"
