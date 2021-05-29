d9bdc94b653d1ddae566d5195ae5459beb3d44d2
javi mugueta <javiermugueta@gmail.com>
2021-05-29 10:50:02 +0200

    Add files via upload

--- deeplist.sh
+++ deeplist.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 # jmugueta may 2021
-# 
+#
+# recursive list of compute instances in tenancy
+# CREATING_IMAGE, MOVING, PROVISIONING, RUNNING, STARTING, STOPPED, STOPPING, TERMINATED, TERMINATING
+#
 recorre(){
     for i in $(oci iam compartment list --compartment-id $1 | jq -r '.data[].id')
     do
         c=$((c+1))
         indent $c
         oci iam compartment get --compartment-id $i | jq -r '.data."name"'
-        oci compute instance list --compartment-id $i --lifecycle-state RUNNING | jq -r '.data[]."display-name"'
+        oci compute instance list --compartment-id $i --lifecycle-state $2 | jq -r '.data[]."display-name"'
         recorre $i 
         c=$((c-1))
     done
@@ -24,5 +27,6 @@ indent() {
   printf "]"
 }
 #
+state=$1
 tenancy=$(oci iam availability-domain list --all | jq -r '.data[0]."compartment-id"')
-recorre $tenancy
+recorre $tenancy $state 
