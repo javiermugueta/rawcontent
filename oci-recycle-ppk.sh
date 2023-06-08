c7da5456ad8f19f9243f5e7f243e5d41dedf7899
Francisco Javier Mugueta Unzue [NE] <null>
2023-06-09 00:04:59 +0200

    .

--- oci-recycle-ppk.sh
+++ oci-recycle-ppk.sh
@@ -34,7 +34,8 @@ fi
 #
 docker pull ghcr.io/oracle/oci-cli:latest
 docker tag ghcr.io/oracle/oci-cli:latest oci
-alias oci='docker run --rm -it -v "$HOME/.oci:/oracle/.oci" oci'
+alias oci='docker run --rm -it -v ".oci:/oracle/.oci" oci'
+#alias oci='docker run --rm -it -v "$HOME/.oci:/oracle/.oci" oci'
 oci os ns get
 # create a new key pair
 openssl genrsa -out $ppkfile 2048       
